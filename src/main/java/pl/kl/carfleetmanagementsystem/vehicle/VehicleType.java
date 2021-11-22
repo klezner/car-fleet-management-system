abf2d67c15bf807b239a1a897da6e4da73e8fdb8
klezner <krzys3ek@gmail.com>
2021-11-22 17:29:44 +0100

    Add common names to vehicle types

--- src/main/java/pl/kl/carfleetmanagementsystem/vehicle/VehicleType.java
+++ src/main/java/pl/kl/carfleetmanagementsystem/vehicle/VehicleType.java
@@ -1,7 +1,17 @@
 package pl.kl.carfleetmanagementsystem.vehicle;
 
 public enum VehicleType {
-    CAR,
-    TRUCK,
-    OTHER
+    CAR("Osobowy"),
+    TRUCK("Ciężarowy"),
+    OTHER("Inny");
+
+    private final String commonName;
+
+    VehicleType(String commonName) {
+        this.commonName = commonName;
+    }
+
+    public String getCommonName() {
+        return commonName;
+    }
 }
