39e5d01b5131949a7b88a0e6b1cd0ae5d40752e3
klezner <krzys3ek@gmail.com>
2021-11-22 12:34:29 +0100

    Implement car controller with car menu endpoint

---
+++ src/main/java/pl/kl/carfleetmanagementsystem/car/CarController.java
@@ -0,0 +1,15 @@
+package pl.kl.carfleetmanagementsystem.car;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/car")
+public class CarController {
+
+    @GetMapping("")
+    public String getCarHomePage() {
+        return "car/index";
+    }
+}
